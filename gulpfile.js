759350feb354054f9c09f7d0b31dc2a40d338145
ndoak <natalieannedoak@gmail.com>
2016-04-06 13:30:30 -0400

    Add uglify plugin to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
 
 gulp.task('js', function() {
   gulp.src(['ng/module.js', 'ng/**/*.js'])
   .pipe(concat('app.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('assets'))
 })

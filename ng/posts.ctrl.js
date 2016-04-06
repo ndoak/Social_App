cea118d178a7930ac75987aa5f8424a0c2f8af34
ndoak <natalieannedoak@gmail.com>
2016-04-06 13:07:14 -0400

    Refactor angular code

---
+++ ng/posts.ctrl.js
@@ -0,0 +1,17 @@
+anuglar.module('app')
+.controller('PostsCtrl', function ($scope, PostsSvc) {
+  $scope.addPost = function () {
+    if ($scope.postBody) {
+      PostsSvc.create({
+        username: 'ndoak',
+        body: $scope.postBody
+      }).success(function (post) {
+        $scope.posts.unshift(post)
+        $scope.postBody = null
+      })
+    }
+  }
+  PostsSvc.fetch().success(function (posts) {
+    $scope.posts = posts
+  })
+})

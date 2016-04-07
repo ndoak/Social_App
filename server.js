017c4b724da5bc71e7eef52454a2b4f8e91a090c
ndoak <natalieannedoak@gmail.com>
2016-04-07 18:42:41 -0400

    Fix controller router and add navbar

--- server.js
+++ server.js
@@ -4,27 +4,8 @@ var bodyParser = require('body-parser')
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/posts', function (req, res, next) {
-  Post.find(function(err, posts) {
-    if (err) { return next(err) }
-    res.json(posts)
-  })
-})
-
-app.get('/', function(req, res) {
-  res.sendfile('templates/posts.html')
-})
-
-app.post('/api/posts', function (req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  })
-  post.save(function (err, post) {
-    if (err) { return next(err) }
-    res.json(201, post)
-  })
-})
+app.use('/api/posts', require('./controllers/api/posts'))
+app.use(require('./controllers/static'))
 
 app.listen(3000, function () {
   console.log('Server is listening on', 3000)

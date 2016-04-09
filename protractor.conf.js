251e0e98d0066940538cb668771b92df1d991742
ndoak <natalieannedoak@gmail.com>
2016-04-09 00:49:41 -0400

    Add config file for protractor

---
+++ protractor.conf.js
@@ -0,0 +1,9 @@
+exports.config = {
+  framework: 'mocha',
+  specs: [
+    'test/e2e/**/*.spec.js'
+  ],
+  mocha0pts: {
+    enableTimeouts: false
+  }
+}
---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,5 @@
+describe('making a post', function () {
+  it('logs in and creates a new post', function () {
+    browser.get('http://localhost:3000')
+  })
+})
